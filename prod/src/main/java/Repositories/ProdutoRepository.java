8ee1da7d86f8c96e81a9228169aa2dc1ba2f48bf
André Perin Geraldo <88738089+andreperinn@users.noreply.github.com>
2026-03-12 21:24:57 -0300

    Estrutura criada

---
+++ prod/src/main/java/Repositories/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package Repositories;
+
+import com.ap.prod.Models.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository <ProdutoModel, Long>{
+}
---
+++ prod/src/main/java/com/ap/prod/Models/ProdutoModel.java
@@ -0,0 +1,54 @@
+package com.ap.prod.Models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_PROD")
+public class ProdutoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+    private Integer quantidadeEmEstoque;
+
+
+    public ProdutoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
+
+
